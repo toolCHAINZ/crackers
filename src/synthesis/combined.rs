e57e18acd0671ccc5d3de5345911587daa18756f
Mark <mark.denhoed@cs.ox.ac.uk>
2024-07-18 11:30:00 +0100

    Add but also disable the filtering

--- src/synthesis/combined.rs
+++ src/synthesis/combined.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use jingle::modeling::ModeledBlock;
 use jingle::sleigh::Instruction;
 use tracing::{event, Level};
@@ -15,30 +17,40 @@ pub struct CombinedAssignmentSynthesis<'a> {
 
 impl<'a> CombinedAssignmentSynthesis<'a> {
     pub fn decide(&mut self) -> Result<DecisionResult<'a, ModeledBlock<'a>>, CrackersError> {
-        let mut ordering: Vec<Vec<Instruction>> = self.base_config.instructions.partitions().map(|part| {
-            part.into_iter()
-                .map(|instrs| Instruction::try_from(instrs).unwrap())
-                .collect::<Vec<Instruction>>()
-        }).collect();
+        let mut ordering: Vec<Vec<Instruction>> = self
+            .base_config
+            .instructions
+            .partitions()
+            .map(|part| {
+                part.into_iter()
+                    .map(|instrs| Instruction::try_from(instrs).unwrap())
+                    .collect::<Vec<Instruction>>()
+            })
+            .collect();
+        let mut blacklist = HashSet::new();
         // todo: gross hack to avoid rewriting the partitioning algorithm to be breadth-first
-        ordering.sort_by(|a,b|a.len().partial_cmp(&b.len()).unwrap());
+        ordering.sort_by(|a, b| a.len().partial_cmp(&b.len()).unwrap());
         let mut iter = ordering.into_iter();
         let mut last: Option<DecisionResult<'a, ModeledBlock<'a>>> = None;
         while let Some(instructions) = iter.next() {
             // todo: filter for instruction combinations that have already been ruled out?
+            if instructions.iter().any(|i| blacklist.contains(i)) {
+                continue;
+            }
             let mut new_config = self.base_config.clone();
             new_config.instructions = instructions;
             let synth = AssignmentSynthesis::new(self.z3, &new_config);
             if let Ok(mut synth) = synth {
-                match synth.decide(){
+                match synth.decide() {
                     Ok(result) => {
-                        match result{
+                        match result {
                             DecisionResult::AssignmentFound(a) => {
                                 return Ok(DecisionResult::AssignmentFound(a).into());
                             }
                             DecisionResult::Unsat(e) => {
                                 // todo: add in bad combos here
                                 event!(Level::WARN, "{:?}", e);
+                                // e.indexes.iter().for_each(|i|{blacklist.insert(new_config.instructions[*i].clone());});
                                 last = Some(DecisionResult::Unsat(e))
                             }
                         }
@@ -53,12 +65,7 @@ impl<'a> CombinedAssignmentSynthesis<'a> {
     }
 
     pub fn new(z3: &'a Context, base_config: SynthesisParams) -> Self {
-
-
-        let res = Self {
-            z3,
-            base_config,
-        };
+        let res = Self { z3, base_config };
         res
     }
 }
