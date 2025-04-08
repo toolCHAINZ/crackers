726e2d8a8ab40c4fe2fef6b5fa7bee8ff13a8b15
Mark <mark.denhoed@cs.ox.ac.uk>
2025-04-08 19:15:08 +0100

    Add some defs

--- crackers_python/src/lib.rs
+++ crackers_python/src/lib.rs
@@ -1,11 +1,12 @@
-use pyo3::prelude::*;
-use ::crackers::config::CrackersConfig;
+use ::crackers::config::constraint::{ConstraintConfig, MemoryEqualityConstraint, PointerRange, PointerRangeConstraints, StateEqualityConstraint};
 use ::crackers::config::meta::{CrackersLogLevel, MetaConfig};
 use ::crackers::config::sleigh::SleighConfig;
 use ::crackers::config::specification::SpecificationConfig;
 use ::crackers::config::synthesis::SynthesisConfig;
+use ::crackers::config::CrackersConfig;
 use ::crackers::gadget::library::builder::GadgetLibraryConfig;
 use ::crackers::synthesis::builder::SynthesisSelectionStrategy;
+use pyo3::prelude::*;
 
 /// A Python module implemented in Rust.
 #[pymodule]
@@ -18,5 +19,10 @@ fn crackers(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<SynthesisConfig>()?;
     m.add_class::<CrackersLogLevel>()?;
     m.add_class::<SynthesisSelectionStrategy>()?;
+    m.add_class::<PointerRange>()?;
+    m.add_class::<MemoryEqualityConstraint>()?;
+    m.add_class::<PointerRangeConstraints>()?;
+    m.add_class::<StateEqualityConstraint>()?;
+    m.add_class::<ConstraintConfig>()?;
     Ok(())
 }
