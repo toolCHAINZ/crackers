8d3cf9723b68d6abb393ba3af6f38e4c4272aefb
Mark <mark.denhoed@cs.ox.ac.uk>
2025-04-11 12:06:53 +0100

    Add in some stuff

--- crackers_python/src/decision/assignment_model.rs
+++ crackers_python/src/decision/assignment_model.rs
@@ -2,7 +2,11 @@ use crackers::synthesis::assignment_model::AssignmentModel;
 use jingle::modeling::{ModeledBlock, ModelingContext};
 use jingle::python::modeled_block::PythonModeledBlock;
 use jingle::python::state::PythonState;
-use pyo3::{pyclass, pymethods};
+use jingle::python::varode_iterator::VarNodeIterator;
+use jingle::python::z3::ast::{TryFromPythonZ3, TryIntoPythonZ3};
+use pyo3::exceptions::PyRuntimeError;
+use pyo3::{pyclass, pymethods, Py, PyAny, PyResult};
+use z3::ast::BV;
 
 #[pyclass(unsendable)]
 pub struct PythonAssignmentModel {
@@ -11,6 +15,17 @@ pub struct PythonAssignmentModel {
 
 #[pymethods]
 impl PythonAssignmentModel {
+
+    fn eval_bv(&self, bv: Py<PyAny>, model_completion: bool) -> PyResult<Py<PyAny>> {
+        let bv = BV::try_from_python(bv)?;
+        let val = self
+            .inner
+            .model()
+            .eval(&bv, model_completion)
+            .ok_or(PyRuntimeError::new_err("Could not eval model"))?;
+        val.try_into_python()
+    }
+
     pub fn initial_state(&self) -> Option<PythonState> {
         self.inner.gadgets.first().map(|f| PythonState {
             state: f.get_original_state().clone(),
@@ -30,5 +45,27 @@ impl PythonAssignmentModel {
             .into_iter()
             .map(|g| PythonModeledBlock { instr: g })
             .collect()
+    }
+
+    pub fn inputs(&self) -> Option<VarNodeIterator> {
+        let state = self.inner.initial_state()?;
+        Some(VarNodeIterator::new(
+            state.clone(),
+            self.inner
+                .gadgets
+                .iter()
+                .flat_map(|g| g.get_inputs().into_iter()),
+        ))
+    }
+
+    pub fn outputs(&self) -> Option<VarNodeIterator> {
+        let state = self.inner.initial_state()?;
+        Some(VarNodeIterator::new(
+            state.clone(),
+            self.inner
+                .gadgets
+                .iter()
+                .flat_map(|g| g.get_outputs().into_iter()),
+        ))
     }
 }
