8ea3f7ba58e713707ba39fe1f4deadce14191f91
Mark <mark.denhoed@cs.ox.ac.uk>
2025-09-19 15:07:14 +0100

    stuff

--- crackers_python/noxfile.py
+++ crackers_python/noxfile.py
@@ -4,10 +4,10 @@ import nox
 def ruff(session):
     """Run ruff linter."""
     session.install("ruff")
-    session.run("ruff", ".")
+    session.run("ruff", "check", "crackers")
 
 @nox.session
 def mypy(session):
     """Run mypy type checker."""
     session.install("mypy")
-    session.run("mypy", ".")
+    session.run("mypy", "crackers")
